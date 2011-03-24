c17a77a4ca932dac2c110e5bd901ff0a300defd1
Raimond Spekking <raymond@users.mediawiki.org>
2011-03-24 21:05:31 +0000

    Localisation updates for core and extension messages from translatewiki.net (2011-03-24 20:39:00 UTC)

--- VipsScaler.i18n.php
+++ VipsScaler.i18n.php
@@ -33,6 +33,13 @@ $messages['de'] = array(
 	'vipsscaler-desc' => 'Ermöglicht das Erstellen von Miniaturansichten mit VIPS',
 );
 
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'vipsscaler-desc' => 'Přehladowe wobrazki z pomocu VIPS wutworić',
+);
+
 /** Interlingua (Interlingua)
  * @author McDutchie
  */
@@ -47,6 +54,13 @@ $messages['ksh'] = array(
 	'vipsscaler-desc' => 'Minibeldsche met <i lang="en">VIPS</i> maache.',
 );
 
+/** Luxembourgish (Lëtzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'vipsscaler-desc' => 'Miniaturbiller mat VIPS maachen',
+);
+
 /** Macedonian (Македонски)
  * @author Bjankuloski06
  */
@@ -54,10 +68,31 @@ $messages['mk'] = array(
 	'vipsscaler-desc' => 'Создавање на минијатури со VIPS',
 );
 
+/** Portuguese (Português)
+ * @author Hamilton Abreu
+ */
+$messages['pt'] = array(
+	'vipsscaler-desc' => 'Criar miniaturas usando VIPS',
+);
+
+/** Russian (Русский)
+ * @author Александр Сигачёв
+ */
+$messages['ru'] = array(
+	'vipsscaler-desc' => 'Создаёт миниатюры с помощью VIPS',
+);
+
 /** Telugu (తెలుగు)
  * @author Veeven
  */
 $messages['te'] = array(
 	'vipsscaler-desc' => 'VIPSని ఉపయోగించి నఖచిత్రాలను తయారుచేయండి',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'vipsscaler-desc' => 'Lumikha ng mga kagyat na ginagamit ang VIPS',
 );
 
